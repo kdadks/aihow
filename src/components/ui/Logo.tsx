251bc2cf2fb529012c83c0d13af509500b13477e
kdadks <kdadks@outlook.com>
2025-06-11 16:28:54 +0100

    Update logo and manifest changes

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -4,7 +4,7 @@ import logo from '../../assets/light logo.png';
 export const Logo: React.FC<{ className?: string }> = ({ className }) => {
   return (
     <div className={`inline-flex items-center ${className}`}>
-      <img src={logo} alt="Logo" className="h-8 w-auto min-h-[32px]" />
+      <img src={logo} alt="Logo" className="h-12 w-auto min-h-[48px]" />
     </div>
   );
 };
